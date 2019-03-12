7abfd70fa8aa98861a7837838a2b622176a017ed
akay.hu[胡凱綺] <akay.hu@104.com.tw>
2019-03-12 17:02:22 +0800

    筆記新增線上學習資源 與 筆記 title 改為中文

--- src/file/file.stories.js
+++ src/file/file.stories.js
@@ -5,10 +5,12 @@ import react from './react.md';
 import storybook from './storybook.md';
 import cleanCode from './cleanCode.md';
 import immutable from './immutable.md';
+import onlineLearning from './onlineLearning.md';
 
 storiesOf('筆記', module)
   .addDecorator(story => <div style={{ padding: '3rem' }}>{story()}</div>)
-  .add('React Development', doc(react))
-  .add('Storybook Development', doc(storybook))
+  .add('安裝 React 開發環境', doc(react))
+  .add('安裝 Storybook 環境', doc(storybook))
   .add('Immutable', doc(immutable))
-  .add('5 Tips to Write Better Conditionals in JavaScript', doc(cleanCode))
+  .add('在JavaScript中編寫更好的條件的5個技巧', doc(cleanCode))
+  .add('線上學習資源', doc(onlineLearning))
---
+++ src/file/onlineLearning.md
@@ -0,0 +1,5 @@
+# 線上學習資源
+
+- Hiskio：[https://hiskio.com/](https://hiskio.com/)
+- Udemy：[https://www.udemy.com/](https://www.udemy.com/)
+- 彭彭的課程：[https://www.youtube.com/channel/UCguZS-y7codLSt6vpkVdnKg](https://www.youtube.com/channel/UCguZS-y7codLSt6vpkVdnKg)
