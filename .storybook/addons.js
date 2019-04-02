e77dcbc88f8200788979e788172c0e2ce9820743
akay.hu[胡凱綺] <akay.hu@104.com.tw>
2019-04-02 16:31:35 +0800

    調整圖片位置

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,5 @@
 import '@storybook/addon-storysource/register';
+import '@storybook/addon-a11y/register';
 import 'storybook-addon-react-docgen/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-viewport/register';
