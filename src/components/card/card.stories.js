82d8e521f8c275af8e8e34af0a6fbe7323949c58
akay.hu[胡凱綺] <akay.hu@104.com.tw>
2019-04-03 16:31:13 +0800

    component Card data抽出

--- src/components/card/card.stories.js
+++ src/components/card/card.stories.js
@@ -2,55 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Card from './card';
 import readme from './readme.md';
-import selfImage from './image/cover.jpg';
-import bgImage from './image/bg.png';
-
-export const data = [
-	{
-		link: 'https://www.google.com',
-		linkTitle: 'Google',
-		defultUserImage: '',
-		defultBgImage: '',
-		userImage: selfImage,
-		bgImage: bgImage,
-		userName: '孫悟空',
-		introduction:
-			'孫悟空，是小說《西遊記》中主要角色之一。又名孫行者，自封花果山美猴王、齊天大聖。曾任天官弼馬溫。玉帝後來為了招安孫悟空，承認了他自封的封號齊天大聖。取經後為如來佛祖授為鬪戰勝佛。',
-	},
-	{
-		link: 'https://www.google.com',
-		linkTitle: 'Google',
-		defultUserImage: '',
-		defultBgImage: '',
-		userImage: selfImage,
-		bgImage: bgImage,
-		userName: '孫悟空',
-		introduction:
-			'孫悟空，是小說《西遊記》中主要角色之一。又名孫行者，自封花果山美猴王、齊天大聖。曾任天官弼馬溫。玉帝後來為了招安孫悟空，承認了他自封的封號齊天大聖。取經後為如來佛祖授為鬪戰勝佛。',
-	},
-	{
-		link: 'https://www.google.com',
-		linkTitle: 'Google',
-		defultUserImage: '',
-		defultBgImage: '',
-		userImage: selfImage,
-		bgImage: bgImage,
-		userName: '孫悟空',
-		introduction:
-			'孫悟空，是小說《西遊記》中主要角色之一。又名孫行者，自封花果山美猴王、齊天大聖。曾任天官弼馬溫。玉帝後來為了招安孫悟空，承認了他自封的封號齊天大聖。取經後為如來佛祖授為鬪戰勝佛。',
-	},
-	{
-		link: 'https://www.google.com',
-		linkTitle: 'Google',
-		defultUserImage: '',
-		defultBgImage: '',
-		userImage: selfImage,
-		bgImage: bgImage,
-		userName: '孫悟空',
-		introduction:
-			'孫悟空，是小說《西遊記》中主要角色之一。又名孫行者，自封花果山美猴王、齊天大聖。曾任天官弼馬溫。玉帝後來為了招安孫悟空，承認了他自封的封號齊天大聖。取經後為如來佛祖授為鬪戰勝佛。',
-	},
-];
+import { data } from './cardData';
 
 const card = storiesOf('UI Components|小名片', module);
 
