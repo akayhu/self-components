259cdc33545d922c1723bc3a27b60fc5061f1918
akay.hu[胡凱綺] <akay.hu@104.com.tw>
2019-04-11 15:48:28 +0800

    筆記新增 px 與 em 轉換

--- src/file/cssFile/cssFile.stories.js
+++ src/file/cssFile/cssFile.stories.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { doc } from 'storybook-readme';
 import rwdSize from './rwdSize.md';
+import pixelToEm from './pixelToEm.md';
 
 const cssFile = storiesOf('筆記|CSS', module);
 
 cssFile
 	.addDecorator(story => <div style={{ padding: '3rem' }}>{story()}</div>)
-	.add('RWD 各尺寸參考', doc(rwdSize), { notes: { markdown: rwdSize } });
+	.add('RWD 各尺寸參考', doc(rwdSize), { notes: { markdown: rwdSize } })
+	.add('px 與 em 轉換', doc(pixelToEm), { notes: { markdown: pixelToEm } });
